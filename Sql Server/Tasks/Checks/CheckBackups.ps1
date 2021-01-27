f9c80005b3d78172ea5702012a5403f0e112d38f
Wintyswilly <Wintyboss@outlook.com>
2021-01-27 18:12:40 +0100

    added backup checking script

---
+++ Sql Server/Tasks/Checks/CheckBackups.ps1
@@ -0,0 +1,42 @@
+﻿# CREATE FUNCTION
+Function CheckDbs
+{
+# IMPORT INSTANCE LIST
+
+clear
+$instances1 = Import-CSV "C:\DBAChecks\InstancesLivNetbackupFailOver.csv" 
+
+#$instances1.target_name
+##################################################################
+
+# LOOP THROUGH CREATE RESULTS ARRAY
+ foreach ($row in $instances1.target_name) { 
+ $LastBackup =   Get-DbaLastBackup -SqlInstance $row | Select-Object * | Out-Gridview
+
+ Remove-Variable LastBackup
+
+}
+
+
+##################################################################
+
+# FORMATING 
+$LastBackup | Format-Table
+
+ 
+######################################################################
+# HOUSE KEEPING
+
+Remove-Variable instances1
+#Remove-Variable LastBackup
+##################################################################REPEAT
+
+}
+$confirmation = Read-Host "This script will check all databases and instances? [y/n]"
+while($confirmation -ne "n" ) 
+{   
+CheckDbs
+         $confirmation = Read-Host "Check again? [y/n]"
+            while ($confirmation -ne 'y') {exit}
+
+}
